238f26d0f99c4a3035069ef0304b02d2747b1dc2
Shai Givati <shai@gmail.com>
2023-06-17 22:15:14 +0300

    general

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String HTTP_REQUEST_GET = "GET";
     public static final String QUESTION_MARK = "?";
     public static final String PARAM_VALUE = "value";
-    public static final String REST_DOMAIN = "http://localhost:9030/";
+    public static final String REST_DOMAIN = "https://elector.b-elect.com/";
 }
