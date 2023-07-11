c52ca05b702349d13f36bac3bc5d64c8e847fe18
ShaiAshCollege464 <81906487+ShaiAshCollege464@users.noreply.github.com>
2023-07-11 18:34:32 +0300

    Update Constants.java

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
     public static final String HTTP_REQUEST_GET = "GET";
     public static final String QUESTION_MARK = "?";
     public static final String PARAM_VALUE = "value";
-    public static final String REST_DOMAIN = "https://elector.b-elect.com/";
+    public static final String REST_DOMAIN = "https://app.seker.live/";
 }
